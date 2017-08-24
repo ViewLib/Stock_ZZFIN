ec94b3b7f78918218df49a003919b755975d5211
yupixiu <woshiyupixiu@163.com>
2017-08-24 18:30:42 +0800

    create

---
+++ StockIOS/Stock/Stock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stock
+//
+//  Created by 王凯 on 2017/8/24.
+//  Copyright © 2017年 stock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StockIOS/Stock/Stock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stock
+//
+//  Created by 王凯 on 2017/8/24.
+//  Copyright © 2017年 stock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
