c21136302a58358947bf5011f52bfc6af78097d1
aa5279aa <5694209@qq.com>
2017-10-07 15:21:50 +0800

    权限验证

---
+++ StockSVC/src/main/java/com/stock/servlet/GetPermissionServlet.java
@@ -0,0 +1,29 @@
+package com.stock.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "GetPermissionServlet")
+public class GetPermissionServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(200);
+        PrintWriter writer = response.getWriter();
+        double random = Math.random();
+        if (random > 0.5) {
+            writer.write("noPermission");
+        } else {
+            writer.write("success");
+        }
+
+        writer.flush();
+    }
+}
