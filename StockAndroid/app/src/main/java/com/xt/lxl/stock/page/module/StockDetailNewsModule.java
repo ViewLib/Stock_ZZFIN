4d13719ffd50d9536a916aab4464e7a7a2a2f4c9
xiangleiliu <xiangleiliu@Ctrip.com>
2017-10-20 21:48:46 +0800

    jindu

---
+++ StockAndroid/app/src/main/java/com/xt/lxl/stock/page/module/StockDetailNewsModule.java
@@ -0,0 +1,38 @@
+package com.xt.lxl.stock.page.module;
+
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.xt.lxl.stock.R;
+import com.xt.lxl.stock.model.model.StockViewModel;
+import com.xt.lxl.stock.widget.view.StockTextView;
+
+/**
+ * Created by Administrator on 2017/10/19 0019.
+ * 重大消息
+ */
+
+public class StockDetailNewsModule extends StockDetailBaseModule {
+
+    private StockTextView mEventTitle;
+    private TextView mLookMore;//查看更多
+    private LinearLayout mEventContainer;//问题列表
+
+
+    public StockDetailNewsModule(StockViewModel stockViewModel) {
+        super(stockViewModel);
+    }
+
+    @Override
+    public void setModuleView(View view) {
+        mEventTitle = (StockTextView) view.findViewById(R.id.stock_important_event_title);
+        mEventContainer = (LinearLayout) view.findViewById(R.id.stock_important_event_list);
+        mLookMore = (TextView) view.findViewById(R.id.stock_detail_event_lookmore);
+    }
+
+    @Override
+    public void bindData(StockViewModel stockViewModel) {
+
+    }
+}
