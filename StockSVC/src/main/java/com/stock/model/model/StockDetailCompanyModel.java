728205773e10a292e91941addb4533e90e315fdd
aa5279aa <5694209@qq.com>
2017-10-22 20:13:11 +0800

    公司详情的model

---
+++ StockSVC/src/main/java/com/stock/model/model/StockDetailCompanyModel.java
@@ -0,0 +1,12 @@
+package com.stock.model.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockDetailCompanyModel {
+
+    public String companyName;//公司名称
+    public String establishDate;//成立时间
+    public String baseArea;//公司总部
+    public String companyBusiness;//主营业务
+}
---
+++ StockSVC/src/main/java/com/stock/model/request/StockDetailCompanyInfoRequest.java
@@ -0,0 +1,9 @@
+package com.stock.model.request;
+
+import com.stock.model.ServiceRequest;
+
+public class StockDetailCompanyInfoRequest extends ServiceRequest {
+    public int serviceCode = 2005;//服务号
+    public String stockCode;//股票代码
+}
+
