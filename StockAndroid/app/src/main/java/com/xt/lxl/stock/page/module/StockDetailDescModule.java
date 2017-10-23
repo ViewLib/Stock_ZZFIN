7ff565745f33396c6ae77ba8e2f75d7912b4925b
xiangleiliu <xiangleiliu@Ctrip.com>
2017-10-23 22:11:09 +0800

    进度提交

--- StockAndroid/app/src/main/java/com/xt/lxl/stock/page/module/StockDetailDescModule.java
+++ StockAndroid/app/src/main/java/com/xt/lxl/stock/page/module/StockDetailDescModule.java
@@ -106,8 +106,8 @@ public class StockDetailDescModule extends StockDetailBaseModule {
         stockHolderTable.addView(row);
         for (int i = 0; i < stockHolderList.size(); i++) {
             StockDetailStockHolder holder = stockHolderList.get(i);
-            row = setRowData(true, holder.stockHolderNmae, holder.stockHolderAmount, holder.stockHolderRatio);
-            stockHolderTable.addView(row, new ViewGroup.LayoutParams(-2, -2));
+            row = setRowData(false, holder.stockHolderNmae, holder.stockHolderAmount, holder.stockHolderRatio);
+            stockHolderTable.addView(row);
             if (i % 2 == 0) {
                 row.setBackgroundColor(Color.parseColor("#FFFFFF"));
             } else {
