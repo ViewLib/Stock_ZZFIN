5d4a81b9e3aff17df832ff75b3f5c28b361cc709
xiangleiliu <xiangleiliu@Ctrip.com>
2017-10-27 15:04:38 +0800

    设置界面

--- StockAndroid/app/src/main/java/com/xt/lxl/stock/page/activity/StockSettingActivity.java
+++ StockAndroid/app/src/main/java/com/xt/lxl/stock/page/activity/StockSettingActivity.java
@@ -3,6 +3,8 @@ package com.xt.lxl.stock.page.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.xt.lxl.stock.R;
+
 /**
  * Created by xiangleiliu on 2017/9/5.
  */
@@ -11,5 +13,9 @@ public class StockSettingActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.stock_setting_layout);
     }
+
+
+
 }
