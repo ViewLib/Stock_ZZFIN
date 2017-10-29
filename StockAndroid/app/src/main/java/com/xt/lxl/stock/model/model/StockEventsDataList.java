93c558b0fa30adab850cc7be33f18d13621a27b8
xiangleiliu <xiangleiliu@Ctrip.com>
2017-10-29 17:28:09 +0800

    重大事件

---
+++ StockAndroid/app/src/main/java/com/xt/lxl/stock/model/model/StockEventsDataList.java
@@ -0,0 +1,16 @@
+package com.xt.lxl.stock.model.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hp on 2017/10/29.
+ */
+public class StockEventsDataList {
+    public final static int TYPE_LIFTED = 1;//是否解禁
+    public final static int TYPE_PLEDGE = 2;//股权质押
+
+    public int eventType;//
+    public String eventName = "";
+    public List<StockEventsDataModel> stockEventsDataModels = new ArrayList<>();
+}
---
+++ StockAndroid/app/src/main/java/com/xt/lxl/stock/model/model/StockEventsDataModel.java
@@ -0,0 +1,23 @@
+package com.xt.lxl.stock.model.model;
+
+/**
+ * Created by hp on 2017/10/29.
+ */
+public class StockEventsDataModel {
+
+    public String eventDate = "";//事件触发的日期
+    public String eventTitle = "";//事件的标题
+    public String eventDesc = "";//事件的描述
+
+    public String attribute1 = "";//
+    public String attribute2 = "";//
+    public String attribute3 = "";//
+    public String attribute4 = "";//
+    public String attribute5 = "";//
+    public String attribute6 = "";//
+    public String attribute7 = "";//
+    public String attribute8 = "";//
+    public String attribute9 = "";//
+    public String attribute10 = "";//
+
+}
