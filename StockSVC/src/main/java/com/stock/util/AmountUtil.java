70445e05e022fcca191e9c192b262a2309eef02c
xiangleiliu <xiangleiliu@Ctrip.com>
2017-11-05 16:45:51 +0800

    万手改成万股，持股比例保留两位小数

--- StockSVC/src/main/java/com/stock/util/AmountUtil.java
+++ StockSVC/src/main/java/com/stock/util/AmountUtil.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
  */
 public class AmountUtil {
 
-    public Float roundedFor(Float value, int rounded) {
+    public static Float roundedFor(Float value, int rounded) {
         BigDecimal b = new BigDecimal(value);
         value = b.setScale(rounded, BigDecimal.ROUND_HALF_UP).floatValue();//小数点后保留4位
         //异常情况
@@ -27,10 +27,11 @@ public class AmountUtil {
 
     public static String transRatioFromHave(String amout) {
         Float aFloat = StringUtil.toFloat(amout);
-        if (aFloat >= 1) {
+        Float bFloat = roundedFor(aFloat, 2);
+        if (bFloat >= 1) {
             return "100%";
         }
-        return aFloat * 100 + "%";
+        return bFloat * 100 + "%";
     }
 
 }
